c8b6c5e8e813bcbbec1dd0ccb3d44f92f47f187d
2379731984@qq.com <2379731984@qq.com>
2024-03-11 18:26:54 +0800

    1

---
+++ 9.ReactivityApi/vue2/$delete.js
@@ -0,0 +1,33 @@
+
+
+
+function del(target, key) {
+
+    // 判断是否是数组 并且 索引是有效索引
+    if (Array.isArray(target) && isValidArrayIndex(key)) {
+        target.splice(key, 1); // 因为数组的原型被重新指向了，方法是被重新的，所以触发了重渲染
+        return;
+    };
+
+    const ob = target.__ob__
+    if (target._isVue || (ob && ob.vmCount)) {
+        // 非根实例或被观察对象，直接返回
+        return
+    }
+
+    if (!hasOwn(target, key)) {
+        // 不存在该属性，直接返回
+        return
+    };
+
+    delete target[key]; // 删除改属性
+
+    if (!ob) {
+        // 没有响应式对象，直接返回
+        return
+    };
+
+
+    // 通知依赖更新
+    ob.dep.notify(); 
+};
--- 9.ReactivityApi/vue2/$set.js
+++ 9.ReactivityApi/vue2/$set.js
@@ -12,24 +12,33 @@ function set(target, key, value) {
     // 判断是否是数组 并且 该属性名是有效的索引
     if (Array.isArray(target) && isValidArrayIndex(key)) {
         target.length = Math.max(target.length, key); // 获取最大值并修改数组的长度
-        target.splice(key, 1, value); 
+        target.splice(key, 1, value); // 因为数组的原型被重新指向了，方法是被重新的，所以触发了重渲染
         return value;
-    }
+    };
+
+    // 判断目标对象中是否有该属性并且不是原型上的属性
     if (key in target && !(key in Object.prototype)) {
-        target[key] = value;
+        target[key] = value;  // 直接更改该属性，让其触发该属性的get函数  
         return value;
-    }
+    };
+
+    // 判断该属性是否是Vue实例或观察者对象
     const ob = (target).__ob__;
     if (target._isVue || (ob && ob.vmCount)) {
-        // 避免在 Vue 实例或观察者对象上添加或修改属性
+        // 非根实例或被观察对象，直接返回
         return value;
-    }
+    };
+
+  
     if (!ob) {
+        // 没有响应式对象，直接赋值
         target[key] = value;
         return value;
-    }
-    defineReactive(ob.value, key, value);
+    };
+
+    defineReactive(ob.value, key, value);  // 对属性进行响应式处理
     ob.dep.notify(); // 派发更新
+
     return value;
 };
 
